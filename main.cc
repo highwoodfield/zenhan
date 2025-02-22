a077479ab53a2a0123235504b258ca3aea31733d
takagino <178282378+highwoodfield@users.noreply.github.com>
2025-02-22 19:41:38 +0900

    ドキュメントを追加

--- main.cc
+++ main.cc
@@ -1,10 +1,19 @@
 #include <cstdio>
 #include <cstdlib>
 #include <windows.h>
+#include <iostream>
 
 int
 main(int argc, char** argv)
 {
+    /*
+    ImmGetContextやImmGetOpenStatusを使うこともできそうに見えたが、
+    ImmGetContextは自分のウィンドウのContextしか取得できないようだ。
+
+    GETOPENSTATUS/SETOPENSTATUSの資料は、以下のWindows CE向けリファレンスしか見つけられなかった。
+    https://learn.microsoft.com/en-us/previous-versions/windows/embedded/ms905960(v=msdn.10)
+    */
+
   constexpr LPARAM IMC_GETOPENSTATUS = 5;
   constexpr LPARAM IMC_SETOPENSTATUS = 6;
 
@@ -18,10 +27,10 @@ main(int argc, char** argv)
 
   LPARAM stat;
   if (argc < 2) {
-    stat = SendMessage(ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, 0);
+      stat = SendMessage(ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, 0);
   } else {
-    stat = std::atoi(argv[1]);
-    SendMessage(ime, WM_IME_CONTROL, IMC_SETOPENSTATUS, stat);
+      stat = std::atoi(argv[1]);
+      SendMessage(ime, WM_IME_CONTROL, IMC_SETOPENSTATUS, stat);
   }
   std::printf("%d\n", stat);
   return 0;
